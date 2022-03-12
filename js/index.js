575f7304961b78245f03aa6313d8ebbfff65cb48
sheharyark017 <sheharyark017@gmail.com>
2022-03-12 12:22:12 +0500

    complete hover animation

--- js/index.js
+++ js/index.js
@@ -31,6 +31,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 10,
   loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
